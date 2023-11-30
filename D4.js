c929dae78ec39fc1f96f991cdfa78f557b979279
Mattia Consiglio <mattiaconsiglio96@gmail.com>
2023-11-30 16:45:39 +0100

    first commit

---
+++ D4.js
@@ -0,0 +1,104 @@
+/* ESERCIZIO 1
+ Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const area = (l1, l2) => l1 * l2
+
+/* ESERCIZIO 2
+ Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
+ la loro somma moltiplicata per tre.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const crazySum = (a, b) => (a === b ? a * 3 : a + b)
+
+/* ESERCIZIO 3
+ Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
+ Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const crazyDiff = a => (a < 19 ? Math.abs(19 - a) : Math.abs(19 - a) * 3)
+console.log(crazyDiff(10))
+console.log(crazyDiff(100))
+
+/* ESERCIZIO 4
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ se n è uguale a 400.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const boundary = n => (n >= 20 && n <= 100) || n === 400
+console.log(boundary(30))
+console.log(boundary(400))
+console.log(boundary(10))
+
+/* ESERCIZIO 5
+ Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ ritornare la stringa originale senza alterarla.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const epify = str => (str.indexOf('EPICODE') === 0 ? str : 'EPICODE' + str)
+console.log(epify('EPICODE'))
+console.log(epify(' è bello'))
+
+/* ESERCIZIO 6
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const check3and7 = n => n >= 0 && (n % 3 === 0 || n % 7 === 0)
+console.log(check3and7(3))
+console.log(check3and7(12))
+console.log(check3and7(15))
+console.log(check3and7(14))
+console.log(check3and7(-14))
+
+/* ESERCIZIO 7
+ Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const reverseString = str => str.split('').reverse().join('')
+console.log(reverseString('EPICODE'))
+
+/* ESERCIZIO 8
+ Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
+ La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const upperFirst = str =>
+	str
+		.split(' ')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ')
+console.log(upperFirst('questa è una frase'))
+
+/* ESERCIZIO 9
+ Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ della stringa originale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const cutString = str => str.slice(1, -1)
+
+/* ESERCIZIO 10
+ Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const giveMeRandom = n => {
+	const array = []
+	for (let i = 0; i < n; i++) {
+		array.push(Math.floor(Math.random() * 11))
+	}
+	return array
+}
+console.log(giveMeRandom(10))
+const giveMeRandomAI = n => Array.from({ length: n }, () => Math.floor(Math.random() * 11))
